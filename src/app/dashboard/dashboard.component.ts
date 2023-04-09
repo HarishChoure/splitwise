e5fd642dac842e44be3b272063e77eafebe0dd1d
Harish Choure <harishchoureoutlook@gmail.com>
2023-04-09 16:48:59 +0530

    Add invite friend button on dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent {
   constructor(private router : Router){}
-  navigateSignup(){
+  navigateInviteFriend(){
     this.router.navigateByUrl('friend/friend-add-edit')
   }
 }
