d2661fef29cf29a837449932e008823aaa49d292
Harish Choure <harishchoureoutlook@gmail.com>
2023-04-10 16:20:37 +0530

    Add shorthand property and remove unnecessary div and use font-size variable and add comment

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,6 +9,7 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent {
   constructor(private router : Router){}
+  //This function is used to navigate the user to friend invite form page
   navigateInviteFriend(){
     this.router.navigateByUrl('friend/friend-add-edit')
   }
